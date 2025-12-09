82fddbb120633801c4dda14e23bfe6d5b7ae172e
Chukwuyenum Opone <oponechukwuyenum@gmail.com>
2025-12-09 22:49:05 +0000

    Initialize Project

---
+++ Source/YenumT3D/Test/ThrowAway.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Test/ThrowAway.h"
+
+ThrowAway::ThrowAway()
+{
+}
+
+ThrowAway::~ThrowAway()
+{
+}
---
+++ Source/YenumT3D/Test/ThrowAway.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class YENUMT3D_API ThrowAway
+{
+public:
+	ThrowAway();
+	~ThrowAway();
+};
